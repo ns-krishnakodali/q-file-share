7db1c88de08755ad505cb2cf8ff92453e732603d
Ujjwal Goel <ugoel@gestaltdiagnostics.com>
2024-10-26 15:57:32 -0400

    Fix: Pass links to display as prop

--- q-file-share-ui/src/app/dashboard/page.tsx
+++ q-file-share-ui/src/app/dashboard/page.tsx
@@ -3,7 +3,13 @@ import { NavBar } from "@/modules";
 const Dashboard = () => {
   return (
     <>
-      <NavBar showLogo={true} currentPage="dashboard" />
+      <NavBar
+        showLogo={true}
+        pages={[
+          { name: "Shared Files", path: "/shared-files" },
+          { name: "Received Files", path: "/received-files" },
+        ]}
+      />
       <div>Secure File Share using Post-Quantum Cryptography</div>
     </>
   );
