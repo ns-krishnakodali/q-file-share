38f5bbbdfef553d8e5d000b49393f8fbdae233f2
Sushanth Reddy <63182782+sushanth-777@users.noreply.github.com>
2024-10-29 23:15:56 -0400

    New: Design dashboard page, correponding modules and SVGs (FE)
    * New: Add dashboard page
    
    * Fix: Remove extra spaces
    
    * Fix
    
    * fix: using existing navbar, included new file for stylying, used svm instead of png
    
    * fix: modified dashboard.css to dashboard.module.css, removed react router and its dependencies
    
    * fix: Re: Renamed css file to dashboard.module.css, removed react dome
    
    * Update: Format Dashboard module and added corresponding modules and svgs
    
    ---------
    
    Co-authored-by: Sushanth Kotha
    Co-authored-by: Krishna Kodali <krishnakodali81@gmail.com>

--- q-file-share-ui/src/app/dashboard/page.tsx
+++ q-file-share-ui/src/app/dashboard/page.tsx
@@ -1,16 +1,58 @@
 "use client";
 
-import { NavBar } from "@/modules";
+import styles from "./dashboard.module.css"
 
-const Dashboard = () => {
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { ActivityCard, Card, NavBar, INotification } from "@/modules";
+import { SEND_FILE, RECEIVED_FILES, SHARED_FILES } from "@/constants";
+
+import sendFileIcon from "@/assets/send-file-icon.svg";
+import receivedFilesIcon from "@/assets/received-files-icon.svg";
+import sharedFilesIcon from "@/assets/shared-files-icon.svg";
+
+
+const Dashboard: React.FC = (): JSX.Element => {
+
+  const router = useRouter();
+  const [notifications, setNotifications] = useState<INotification[]>([]);
+
   return (
     <>
-      <NavBar
-        showLogo={true}
-        pageName="Shared Files"
-        pageURL="/shared-files"
-      />
-      <div>Secure File Share using Post-Quantum Cryptography</div>
+      <NavBar showLogo={true}/>
+      <div className={styles.dashboardContainer}>
+        <div className={styles.cardContainer}>
+          <Card
+            id="share-file-card"
+            className={styles.card}
+            src={sendFileIcon}
+            title={SEND_FILE}
+            description="Securely share your files"
+            onClickHandler={() => {}}
+          />
+          <Card
+            id="received-files-card"
+            className={styles.card}
+            src={receivedFilesIcon}
+            title={RECEIVED_FILES}
+            description="View files you've received"
+            onClickHandler={() => {}}
+          />
+          <Card
+            id="transferred-files-card"
+            className={styles.card}
+            src={sharedFilesIcon}
+            title={SHARED_FILES}
+            description="Manage your shared files"
+            onClickHandler={() => {}}
+          />
+        </div>
+        <div className={styles.activityContainer}>
+          <ActivityCard notifications={notifications}
+          />
+        </div>
+      </div>
     </>
   );
 };
