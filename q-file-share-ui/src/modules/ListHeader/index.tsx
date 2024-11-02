9dad1181a24b9db62aa0a970ee5eab73c25fdaa5
talari30 <talarirahul30@gmail.com>
2024-11-01 23:11:45 -0400

    Update: Format modified files

--- q-file-share-ui/src/modules/ListHeader/index.tsx
+++ q-file-share-ui/src/modules/ListHeader/index.tsx
@@ -1,25 +1,27 @@
 import Image from "/Users/talarirahul/Desktop/CNS/q-file-share/q-file-share-ui/node_modules/next/image";
-import styles from './ListHeader.module.css';
-import Filter from '@/assets/filter.svg';
-import {Button} from  '/Users/talarirahul/Desktop/CNS/q-file-share/q-file-share-ui/src/elements/Button';
-import {Text} from  '/Users/talarirahul/Desktop/CNS/q-file-share/q-file-share-ui/src/elements/Text';
+import styles from "./ListHeader.module.css";
+import Filter from "@/assets/filter.svg";
+import { Button } from "/Users/talarirahul/Desktop/CNS/q-file-share/q-file-share-ui/src/elements/Button";
+import { Text } from "/Users/talarirahul/Desktop/CNS/q-file-share/q-file-share-ui/src/elements/Text";
 
 interface ListHeaderProps {
-    title: string; 
-    
+  title: string;
 }
 export const ListHeader: React.FC<ListHeaderProps> = ({ title }) => {
-    return (
-        <div className={styles.header}>
-            <div className={styles.title}>
-                {title}
-            </div>
-            <Button id="button1" type="button" variant = "text" className={styles.filter}>
-                <Image src={Filter} alt="Filter" className={styles.logo} />
-                <div className={styles.title}>
-                    <Text>Filter</Text>
-                </div>
-            </Button>
+  return (
+    <div className={styles.header}>
+      <div className={styles.title}>{title}</div>
+      <Button
+        id="button1"
+        type="button"
+        variant="text"
+        className={styles.filter}
+      >
+        <Image src={Filter} alt="Filter" className={styles.logo} />
+        <div className={styles.title}>
+          <Text>Filter</Text>
         </div>
-    );
+      </Button>
+    </div>
+  );
 };
