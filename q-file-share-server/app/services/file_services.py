1ea0c456b9233e091a61982e2305b06771094800
Krishna Kodali <85895093+KrishnaKodali99@users.noreply.github.com>
2024-11-28 14:54:09 -0500

    New: Implement file activities API (BE)
    * Fix: Rebase and resolve conflicts with dev (BE)
    
    * Update: Integrate with existing file API changes
    
    * Fix: Minor code changes (BE)
    
    ---------
    
    Co-authored-by: talari30 <talarirahul30@gmail.com>

--- q-file-share-server/app/services/file_services.py
+++ q-file-share-server/app/services/file_services.py
@@ -2,10 +2,31 @@ import datetime
 
 from app.db.db_session import get_db_session
 from app.models.db_models import FileLog
-from app.models.response_models import ReceivedFilesResponse, SharedFilesResponse
+from app.models.response_models import (
+    ActivitiesResponse,
+    ReceivedFilesResponse,
+    SharedFilesResponse,
+)
 
 
-def retrieve_received_files(email: str) -> str:
+def get_files_actitvity(user_email: str):
+    db = next(get_db_session())
+    files = (
+        db.query(FileLog)
+        .filter((FileLog.from_email == user_email) | (FileLog.to_email == user_email))
+        .order_by(FileLog.sent_on.desc())
+        .limit(10)
+        .all()
+    )
+    return [
+        ActivitiesResponse(
+            name=file.name, received_from=file.from_email, sent_to=file.to_email
+        )
+        for file in files
+    ]
+
+
+def retrieve_received_files(user_email: str) -> str:
     db = next(get_db_session())
     files = (
         db.query(
@@ -17,7 +38,7 @@ def retrieve_received_files(email: str) -> str:
             FileLog.is_anonymous,
         )
         .filter(
-            FileLog.to_email == email,
+            FileLog.to_email == user_email,
             FileLog.status == "active",
             FileLog.expiry > datetime.datetime.now(),
         )
@@ -29,14 +50,14 @@ def retrieve_received_files(email: str) -> str:
             name=file.name,
             size=file.size,
             received_on=file.received_on,
-            received_from=file.received_from if not file.is_anonymous else None,
+            received_from=file.from_email if not file.is_anonymous else None,
             expiry=file.expiry,
         )
         for file in files
     ]
 
 
-def retrieve_shared_files(email: str) -> str:
+def retrieve_shared_files(user_email: str) -> str:
     db = next(get_db_session())
     files = (
         db.query(
@@ -48,7 +69,7 @@ def retrieve_shared_files(email: str) -> str:
             FileLog.is_anonymous,
         )
         .filter(
-            FileLog.from_email == email,
+            FileLog.from_email == user_email,
             FileLog.status == "active",
             FileLog.expiry > datetime.datetime.now(),
         )
@@ -60,7 +81,7 @@ def retrieve_shared_files(email: str) -> str:
             name=file.name,
             size=file.size,
             sent_on=file.received_on,
-            sent_to=file.received_from if not file.is_anonymous else None,
+            sent_to=file.to_email if not file.is_anonymous else None,
             expiry=file.expiry,
         )
         for file in files
