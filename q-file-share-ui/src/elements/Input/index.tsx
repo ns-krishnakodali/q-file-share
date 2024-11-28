3a0b941ee5efe6b5ad8b75b4aff2de1981144ded
Krishna Kodali <krishnakodali81@gmail.com>
2024-11-27 21:32:32 -0500

    New: Reformat using prettier and update gitignore

--- q-file-share-ui/src/elements/Input/index.tsx
+++ q-file-share-ui/src/elements/Input/index.tsx
@@ -17,34 +17,36 @@ interface IInputProps {
 
 import React, { forwardRef } from "react";
 
-export const Input = forwardRef<HTMLInputElement, IInputProps>((props, ref): JSX.Element => {
-  const {
-    id,
-    className,
-    name = "",
-    type = "text",
-    value,
-    placeholder = "",
-    required = false,
-    readOnly = false,
-    onClickAction,
-  } = props;
+export const Input = forwardRef<HTMLInputElement, IInputProps>(
+  (props, ref): JSX.Element => {
+    const {
+      id,
+      className,
+      name = "",
+      type = "text",
+      value,
+      placeholder = "",
+      required = false,
+      readOnly = false,
+      onClickAction,
+    } = props;
 
-  return (
-    <div className={className}>
-      <input
-        id={id}
-        name={name}
-        className={styles.input}
-        ref={ref}
-        type={type}
-        value={value}
-        placeholder={type !== "checkbox" ? placeholder : ""}
-        required={required}
-        readOnly={readOnly}
-        onClick={onClickAction}
-      />
-      {type === "checkbox" && <label htmlFor={id}>{placeholder}</label>}
-    </div>
-  );
-});
+    return (
+      <div className={className}>
+        <input
+          id={id}
+          name={name}
+          className={styles.input}
+          ref={ref}
+          type={type}
+          value={value}
+          placeholder={type !== "checkbox" ? placeholder : ""}
+          required={required}
+          readOnly={readOnly}
+          onClick={onClickAction}
+        />
+        {type === "checkbox" && <label htmlFor={id}>{placeholder}</label>}
+      </div>
+    );
+  },
+);
